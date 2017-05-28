7fcef881dafe099eabf3ef04d8d39b8817f3ca36
Patryk Ptasiński <patryk.ptasinski@ipepe.pl>
2017-05-28 14:31:03 +0200

    Added geojson meta

--- app/services/wifi_geolocate_service.rb
+++ app/services/wifi_geolocate_service.rb
@@ -2,20 +2,27 @@ class WifiGeolocateService
   def self.query(params)
     return nil if params[:wifiAccessPoints].blank?
     #TODO: save all params for future reference?
-    params[:wifiAccessPoints].sort_by { |ap| ap[:signalStrength] }.reverse.detect do |ap|
+    result = {
+      "location": {
+        "lat": nil,
+        "lng": nil
+      },
+      "accuracy": nil,
+      meta: []
+    }
+    params[:wifiAccessPoints].sort_by { |ap| ap[:signalStrength] }.reverse.each do |ap|
       if (observation = WifiObservation.
         where(bssid: WifiObservation.standardize_bssid(ap[:macAddress])).
         order(observed_at: :desc).
         first).present?
-
-        return {
-          "location": {
-            "lat": observation.latitude,
-            "lng": observation.longitude
-          },
-          "accuracy": observation.geolocation_accuracy
-        }
+        if result["accuracy"].blank?
+          result["location"]["lat"] ||= observation.latitude
+          result["location"]["lng"] ||= observation.longitude
+          result["accuracy"] ||= observation.geolocation_accuracy
+        end
+        result["meta"].push(observation.geojson_hash)
       end
     end
+    result
   end
 end
